9773c7fdcaccc41f13faa3371c1a6144b8eb0db8
Aaliyah <40181569+symlara@users.noreply.github.com>
2022-05-24 11:34:03 -0500

    added app styles, updated user code

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,10 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,8 +16,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-  export const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app); 
-  export const db = getFirestore(app);
-  export const storage = getStorage(app);
+export { auth, db, storage };
